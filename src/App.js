19da731a52a4c5d550b27f19d24ec0bdd4dfd758
Morning <morning@mstars-lab3-05s-iMac.local>
2023-01-03 12:36:06 +0800

    figma

--- src/App.js
+++ src/App.js
@@ -1,24 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  let logo = {img:require('./image/Leap.png')}
+  let nav = [
+    "Portfolio",
+    "Blog",
+    "CV",
+    "Store",
+    "Freelance",
+    "About me",
+    "Contact",
+  ];
+  let minorMenu = [
+    "All",
+    "UI Design",
+    "UX Design",
+    "Product Design",
+    "Aricles",
+    "Tutorials",
+    "News",
+  ];
+
+  let cards = {
+    date: "Nov 23 2020",
+    titleCard: "This way is wrong about UI Design",
+    anchor:
+      "A quick guide to assisting users in the challenging steps from learning about your podcast on the web.  A quick guide to assisting users in thechallenging steps from learning about your podcast on the web. ",
+    read: "READ MORE"
+    };
+  let aar = [];
+  for (let i = 0; i < 6; i++) {
+    aar.push(cards);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="container-xxl">
+      <header className="d-flex m-auto justify-content-between my-4">
+        <div>
+          <img src={logo.img} alt=""/>
+        </div>
+        <ul className="d-flex gap-5">
+          {nav.map((a) => {
+            return <li className="list-unstyled text-secondary">{a}</li>;
+          })}
+        </ul>
       </header>
-    </div>
+      <section>
+        <div>
+          <div>
+            <img src="" alt="sas" />
+            <h1>jsjs</h1>
+          </div>
+          <div>
+            <img src="" alt="sas" />
+            <h1>jsjs</h1>
+          </div>
+          <div>
+            <img src="" alt="sas" />
+            <h1>jsjs</h1>
+          </div>
+        </div>
+        <div>
+          {aar.map((e) => {
+            return (
+              <div>
+                {e.date}
+                {e.titleCard}
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    </main>
   );
 }
 
